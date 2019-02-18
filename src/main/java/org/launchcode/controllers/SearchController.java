8e12aca28d46580c4b07b61cdfcbb457699cddb0
Allen <allen_c_britt@yahoo.com>
2019-02-18 16:43:31 -0500

    tested working

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -3,7 +3,9 @@ package org.launchcode.controllers;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -15,13 +17,40 @@ import java.util.HashMap;
 @Controller
 @RequestMapping("search")
 public class SearchController {
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public SearchController () {
+        columnChoices.put("core competency", "Skill");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("position type", "Position Type");
+        columnChoices.put("all", "All");
+    }
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", SearchController.columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+            model.addAttribute("columns", SearchController.columnChoices);
+            ArrayList<HashMap<String, String>> jobsbyval = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobsbyval);
+            return "search" + "";
+        }
+    }
 
 }
